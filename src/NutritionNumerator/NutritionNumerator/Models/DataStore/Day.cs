fe78241468e671d76f5edb051297eab7a950541e
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-22 02:20:54 +1100

    imp(DataStore): Add model

---
+++ src/NutritionNumerator/NutritionNumerator/Models/DataStore/Day.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+
+namespace NutritionNumerator.Models.DataStore
+{
+    public class Day
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Unique]
+        public DateTime Date { get; set; }
+        public float EnergykCal { get; set; }
+        public float Protein { get; set; }
+        public float Carbohydrates { get; set; }
+        public float Fat { get; set; }
+    }
+}
