e73b589feaec63d432cb754b9d4e71f3c4085b73
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-24 15:39:47 +1100

    build: Update package and display names

--- src/NutritionNumerator/NutritionNumerator.Android/MainActivity.cs
+++ src/NutritionNumerator/NutritionNumerator.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace NutritionNumerator.Droid
 {
-    [Activity(Label = "NutritionNumerator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Nutrition Numerator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
